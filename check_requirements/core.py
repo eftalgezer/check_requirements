953af668d443625f787023c030fed234930de076
Eftal Gezer <eftalgezer@gmail.com>
2023-08-10 19:16:16 +0300

    Add files via upload

--- check_requirements/core.py
+++ check_requirements/core.py
@@ -200,8 +200,13 @@ def find_missing_pkgs(deps_a, deps_b, ignored_pkgs):
     for pkg_a in deps_a:
         pkg_name = pkg_a["name"]
         pkg_version = pkg_a.get("version")
-        if any((pkg_name == ignored["name"] and pkg_version == ignored.get("version")) for ignored in
-               ignored_pkgs):
+        if \
+                any(
+                    (
+                            pkg_name == ignored["name"] and
+                            ((pkg_version == ignored.get("version")) if ignored.get("version") != "" else True))
+                    for ignored in ignored_pkgs
+                ):
             continue
         if not is_pkg_in_subtree(pkg_a, deps_b):
             missing_pkgs.append(pkg_a)
