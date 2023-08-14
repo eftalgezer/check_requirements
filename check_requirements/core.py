1c3ee557274a89efed06c83f7347c41667f4653b
Eftal Gezer <eftalgezer@gmail.com>
2023-08-14 03:27:09 +0300

    Add files via upload

--- check_requirements/core.py
+++ check_requirements/core.py
@@ -103,7 +103,7 @@ def add_info(deps, **kwargs):
             keys = [key for key, val in pkg.items()]
             keys.insert(keys.index("deps") - 1, key)
             pkg[key] = val
-            deps[deps.index(pkg)] = {pkg[key] for key in keys}
+            deps[deps.index(pkg)] = {key: pkg[key] for key in keys}
         add_info(pkg["deps"])
     return deps
 
